2c696959d57ce3f1af3aa7f23ad5992e7e505a2a
Shen Li <dustet@gmail.com>
2021-10-29 03:04:54 +0000

    change postgres hostname in tests

--- tests/settings.py
+++ tests/settings.py
@@ -18,7 +18,7 @@ if os.environ.get("TEST_WITH_PG"):
             "NAME": os.environ["POSTGRES_DB"],
             "USER": os.environ["POSTGRES_USER"],
             "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-            "HOST": "127.0.0.1",
+            "HOST": os.environ["POSTGRES_HOST"],
             "PORT": "5432",
         }
     }
