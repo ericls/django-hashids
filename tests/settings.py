2e931b3395ce96f2b382fcd1fa0f555d14e67bb4
Shen Li <dustet@gmail.com>
2021-10-29 00:21:57 -0400

    test with postgresql (#22)
    There are some backend specific issues, adding this test so we can be more confident in the future

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY = "1"
 DEBUG = True
 INSTALLED_APPS = [
@@ -9,3 +11,14 @@ MIDDLEWARE = []
 ROOT_URLCONF = "tests.urls"
 DJANGO_HASHIDS_SALT = "???!"
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:",}}
+if os.environ.get("TEST_WITH_PG"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["POSTGRES_DB"],
+            "USER": os.environ["POSTGRES_USER"],
+            "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+            "HOST": os.environ["POSTGRES_HOST"],
+            "PORT": "5432",
+        }
+    }
