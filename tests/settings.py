6c9836fa5eb983dade22c1193ffbd7478bf7c186
Shen Li <dustet@gmail.com>
2022-10-20 18:55:54 -0400

    fix MYSQL_DB -> MYSQL_DATABASE

--- tests/settings.py
+++ tests/settings.py
@@ -31,7 +31,7 @@ elif os.environ.get("TEST_WITH_MYSQL"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
-            "NAME": os.environ["MYSQL_DB"],
+            "NAME": os.environ["MYSQL_DATABASE"],
             "USER": os.environ["MYSQL_USER"],
             "PASSWORD": os.environ["MYSQL_PASSWORD"],
             "HOST": os.environ["MYSQL_HOST"],
